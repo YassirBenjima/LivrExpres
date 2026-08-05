b17bc190cc486217ffb051485dacff78ba783a73
YassirBenjima <yassirbenjima@gmail.com>
2026-08-05 15:24:18 +0100

    fix(frontend): separate LanguageProvider to fix Fast Refresh warning and remove unused var warnings

--- livrexp_frontend/src/main.jsx
+++ livrexp_frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { LanguageProvider } from './context/LanguageContext'
+import { LanguageProvider } from './context/LanguageProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
